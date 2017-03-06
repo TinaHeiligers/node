e7d1511fa5f1c3e9da28785e1f5ca8369167d799
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-06 11:54:00 -0500

    implemented wc

--- commands.js
+++ commands.js
@@ -3,48 +3,30 @@ var fs = require('fs');
 module.exports = {
 
   echo: function(args){
-      process.stdout.write(args);
-      process.stdout.write("\nprompt > ");
-
-
+    process.stdout.write(args);
+    process.stdout.write("\nprompt > ");
   },
   
   pwd: function(file) {
-    // process.stdout.write('prompt(in pwd) > ');
-    // process.stdin.on('data', function (data) {
-      // var cmd = data.toString().trim();
-      // if (cmd==="pwd"){
-        var path=process.cwd();
-        process.stdout.write(path);
-        process.stdout.write("\nprompt > ");
-      // }
-    // });
+    var path=process.cwd();
+    process.stdout.write(path);
+    process.stdout.write("\nprompt > ");
   },
 
   date: function(file) {
-    // process.stdin.on('data', function (data) {
-    // var cmd = data.toString().trim(); // remove the newline
-    // if (cmd==="date"){
-      var date=Date();
-      process.stdout.write(date);
-      process.stdout.write("\nprompt > ");
-    // }
+    var date=Date();
+    process.stdout.write(date);
+    process.stdout.write("\nprompt > ");
   },
 
   ls: function(file) {
-    // process.stdout.write('prompt > ');
-    // process.stdin.on('data', function (data) {
-      // var cmd = data.toString().trim();
-      // if (cmd === "ls") {
-        fs.readdir('.', function(err, files) {
-          if (err) throw err;
-          files.forEach(function(file) {
-            process.stdout.write(file.toString() + "\n");
-          })
-          process.stdout.write("prompt > ");
-        });
-      // }
-    // });
+    fs.readdir('.', function(err, files) {
+      if (err) throw err;
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n");
+      })
+      process.stdout.write("prompt > ");
+    });
   },
 
   cat: function(file) {
@@ -73,7 +55,15 @@ module.exports = {
       process.stdout.write(dataArr.slice(start,end).join("\n"));
       process.stdout.write("\nprompt > ");
     });
-}
-
-
+  },
+  wc: function(file) {
+    fs.readFile(file, (err, data) => {
+      if (err) throw err;
+      var dataArr=data.toString().split("\n");
+      var countlines = dataArr.length;
+      
+      process.stdout.write(String(countlines));
+      process.stdout.write("\nprompt > ");
+    });
+  }
 }
