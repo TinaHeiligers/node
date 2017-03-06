2ae95ee66baeaac5aa4103a0446b94f18ffdf9f7
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-06 10:23:58 -0500

    got ls to work

--- bash.js
+++ bash.js
@@ -13,27 +13,3 @@ process.stdin.on('data', function (data) {
   // head and tail prints first/last 5 lines
   commands[cmd]();    
 });
-
-// var userCommand = 'pwd';
-// commands[userCommand]();
-
-// var userComLS = 'ls';
-// commands[userComLS]();
-
-// var date = require('./date');
-// var userComDate = 'date';
-// date[userComDate]();
-
-
-
-// // Output a prompt
-// process.stdout.write('prompt > ');
-// //console.log(Object.keys(process));
-// // The stdin 'data' event fires after a user types in a line
-
-
-// node-shell git:(master) cat bash.js
-// converts everything in the file to a string and prints those out, with a '%' at the end
-
-// ➜  node-shell git:(master) head bash.js
-// /prints only first 10 lines, with a '%' at the end
